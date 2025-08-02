7c390937fa5e23b0851f7519aa688a56118ab8e3
Ishan Veer <ishanpveer@gmail.com>
2025-08-03 00:33:38 +0530

    made home page desktop responsive

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -12,19 +12,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" px-6 md:px-12 py-10 md:py-16">
+    <div className=" px-6 md:px-12 lg:px-60 py-10 md:py-16 lg:py-20">
       {/* heading */}
       <div className="flex justify-between items-center w-full">
         <div className="">
-          <h2 className="hm-bold">Invoices</h2>
-          <p className="md:hidden body text-muted-blues-200_muted-blues-100 pt-3 md:pt-4">
+          <h2 className="hm-bold md:hl-bold">Invoices</h2>
+          <p className="md:hidden body text-muted-blues-200_muted-blues-100 pt-3">
             {7} invoices
           </p>
-          <p className="max-md:hidden body text-muted-blues-200_muted-blues-100 pt-3">
+          <p className="max-md:hidden body text-muted-blues-200_muted-blues-100">
             There are 7 total invoices
           </p>
         </div>
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-5 md:gap-10">
           <DropdownMenu>
             <DropdownMenuTrigger className="flex gap-3">
               <p className="md:hidden text-dark-100_light-100 capitalize hs-bold-variant">
@@ -98,7 +98,7 @@ export default function Home() {
         </div>
       </div>
       {/* invoice list */}
-      <div className="flex flex-col gap-4 py-8">
+      <div className="flex flex-col gap-4 py-8 md:py-16 lg:py-[70px]">
         {invoiceData && invoiceData.length > 0 ? (
           invoiceData.map((invoice) => (
             <InvoiceCard key={invoice.id} invoice={invoice} />
