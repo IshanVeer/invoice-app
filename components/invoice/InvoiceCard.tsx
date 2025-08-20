0f54a9ebfbbd022a3c704629620416b020c8a6cd
Ishan Veer <ishanpveer@gmail.com>
2025-08-20 15:24:13 +0530

    changed invoice card ui

--- components/invoice/InvoiceCard.tsx
+++ components/invoice/InvoiceCard.tsx
@@ -18,41 +18,44 @@ const InvoiceCard = ({ invoice }: InvoiceDataProps) => {
         <p className="text-muted-blues-300_muted-blues-100 body-variant order-3 md:order-2 max-md:self-end ">
           {`Due ${formatDate(invoice.paymentDue)}`}
         </p>
-        <div
-          className={`order-4 md:order-5  flex items-center justify-center gap-2 w-[104px] h-[40px]  rounded-[6px] hs-bold-variant capitalize  justify-self-end self-center row-span-2 ${
-            invoice.status === "paid"
-              ? "bg-[#33d69f]/6 text-[#33d69f]"
-              : invoice.status === "pending"
-              ? "bg-[#ff8f00]/6 text-[#ff8f00]"
-              : invoice.status === "draft"
-              ? "bg-[#373b53]/6 dark:bg-[#dee2fa]/6 text-[#373b53] dark:text-[#dee2fa]"
-              : ""
-          }`}
-        >
-          <div
-            className={`w-2 h-2 rounded-full ${
-              invoice.status === "paid"
-                ? "bg-[#33d69f]"
-                : invoice.status === "pending"
-                ? "bg-[#ff8f00]"
-                : invoice.status === "draft"
-                ? "bg-[#373b53] dark:bg-[#dee2fa]"
-                : ""
-            } `}
-          ></div>
-          {invoice.status}
-        </div>
+
         <p className="hs-bold text-dark-100_light-100 order-5 md:order-4 ">
           {`£ ${invoice.total}`}
         </p>
 
-        <Image
-          src="/assets/icon-arrow-right.svg"
-          alt="link"
-          height={10}
-          width={10}
-          className="order-6 max-md:hidden"
-        />
+        <div className="flex items-center gap-4 order-4 md:order-5 justify-self-end self-center row-span-2">
+          <div
+            className={`  flex items-center justify-center gap-2 w-[104px] h-[40px]  rounded-[6px] hs-bold-variant capitalize    ${
+              invoice.status === "paid"
+                ? "bg-[#33d69f]/6 text-[#33d69f]"
+                : invoice.status === "pending"
+                ? "bg-[#ff8f00]/6 text-[#ff8f00]"
+                : invoice.status === "draft"
+                ? "bg-[#373b53]/6 dark:bg-[#dee2fa]/6 text-[#373b53] dark:text-[#dee2fa]"
+                : ""
+            }`}
+          >
+            <div
+              className={`w-2 h-2 rounded-full ${
+                invoice.status === "paid"
+                  ? "bg-[#33d69f]"
+                  : invoice.status === "pending"
+                  ? "bg-[#ff8f00]"
+                  : invoice.status === "draft"
+                  ? "bg-[#373b53] dark:bg-[#dee2fa]"
+                  : ""
+              } `}
+            ></div>
+            {invoice.status}
+          </div>
+          <Image
+            src="/assets/icon-arrow-right.svg"
+            alt="link"
+            height={10}
+            width={10}
+            className="order-6 max-md:hidden"
+          />
+        </div>
       </div>
     </Link>
   );
